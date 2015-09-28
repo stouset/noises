910a8b4eb81afc67f60b94dd54b52e9bf7162203
Stephen Touset <stephen@touset.org>
2015-09-28 23:53:25 +0000

    Documentation for commit d8eb43a80ae2f8fed4946d70fb2c61fabeefc67f

--- noises/sodium/ffi/sidebar-items.js
+++ noises/sodium/ffi/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["c_void","Type used to construct void pointers for use with C."]],"type":[["c_int",""],["size_t",""]]});
+initSidebarItems({"enum":[["c_void","Type used to construct void pointers for use with C."]],"type":[["c_int",""],["c_ulonglong",""]]});
